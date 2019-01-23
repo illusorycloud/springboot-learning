a185ffbe57a60131a64392f17629573280ee7286
lillusory <xueduanli@163.com>
2019-01-24 00:30:44 +0800

    RabbitMQ 交换器 direct模式

--- demomq/provider/src/test/java/com/example/provider/ProviderApplicationTests.java
+++ demomq/provider/src/test/java/com/example/provider/ProviderApplicationTests.java
@@ -2,6 +2,7 @@ package com.example.provider;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,9 +10,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ProviderApplicationTests {
 
+    @Autowired
+    private Sender sender;
+    
     @Test
-    public void contextLoads() {
+    public void send(){
+        sender.send("Provider");
     }
-
 }
 
