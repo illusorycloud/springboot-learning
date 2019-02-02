9de2e2c512ea2998a4156a383a28ac6143d5661b
lillusory <xueduanli@163.com>
2019-02-02 23:34:23 +0800

    Junit使用

--- eureka/eureka-provider/src/test/java/com/example/eurekaprovider/EurekaProviderApplicationTests.java
+++ eureka/eureka-provider/src/test/java/com/example/eurekaprovider/EurekaProviderApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.eurekaprovider;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,16 @@ public class EurekaProviderApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Before
+    public void before() {
+        System.out.println("before 初始化工作");
+    }
+
+    @After
+    public void after() {
+        System.out.println("after 日志记录");
     }
 
 }
