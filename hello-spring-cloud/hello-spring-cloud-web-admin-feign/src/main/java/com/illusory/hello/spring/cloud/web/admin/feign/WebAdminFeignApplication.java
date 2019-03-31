43b48cb86c62251b0e9c1bb7d6db3e6eac1150f7
lillusory <xueduanli@163.com>
2019-03-31 15:30:33 +0800

    服务熔断监控中心

--- hello-spring-cloud/hello-spring-cloud-web-admin-feign/src/main/java/com/illusory/hello/spring/cloud/web/admin/feign/WebAdminFeignApplication.java
+++ hello-spring-cloud/hello-spring-cloud-web-admin-feign/src/main/java/com/illusory/hello/spring/cloud/web/admin/feign/WebAdminFeignApplication.java
@@ -3,6 +3,7 @@ package com.illusory.hello.spring.cloud.web.admin.feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
@@ -13,6 +14,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
 public class WebAdminFeignApplication {
     public static void main(String[] args) {
         SpringApplication.run(WebAdminFeignApplication.class, args);
--- hello-spring-cloud/hello-spring-cloud-web-admin-feign/src/main/java/com/illusory/hello/spring/cloud/web/admin/feign/service/AdminService.java
+++ hello-spring-cloud/hello-spring-cloud-web-admin-feign/src/main/java/com/illusory/hello/spring/cloud/web/admin/feign/service/AdminService.java
@@ -1,5 +1,6 @@
 package com.illusory.hello.spring.cloud.web.admin.feign.service;
 
+import com.illusory.hello.spring.cloud.web.admin.feign.hystrix.AdminServiceHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
