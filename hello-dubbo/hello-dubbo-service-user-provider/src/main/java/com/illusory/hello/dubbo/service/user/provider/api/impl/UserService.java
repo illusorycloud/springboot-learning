786659bf7432de4c48cdfa59450918c369f61639
李学端 <lixueduan@cqkct.com>
2019-04-01 16:49:21 +0800

    fix：新增负载均衡

--- hello-dubbo/hello-dubbo-service-user-provider/src/main/java/com/illusory/hello/dubbo/service/user/provider/api/impl/UserService.java
+++ hello-dubbo/hello-dubbo-service-user-provider/src/main/java/com/illusory/hello/dubbo/service/user/provider/api/impl/UserService.java
@@ -2,6 +2,8 @@ package com.illusory.hello.dubbo.service.user.provider.api.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 
+import org.springframework.beans.factory.annotation.Value;
+
 /**
  * @author Administrator
  * @version 1.0.0
@@ -9,8 +11,11 @@ import com.alibaba.dubbo.config.annotation.Service;
  */
 @Service(version = "${user.service.version}")
 public class UserService implements com.illusory.hello.dubbo.service.user.api.UserService {
+    @Value("${dubbo.protocol.port}")
+    private String prot;
+
     @Override
     public String sayHi() {
-        return "Hello Dubbo!";
+        return String.format("Hello Dubbo! I am from port: %s", prot);
     }
 }
